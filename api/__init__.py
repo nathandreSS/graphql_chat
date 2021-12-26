f0df01bab404d34ee1f01214782ca6d9f3afabee
Nathan André <nathan_99_@hotmail.com>
2021-12-26 17:40:46 -0300

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -13,8 +13,8 @@ app.config['MONGODB_SETTINGS'] = {
 
 app.config['MAIL_SERVER']= 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'nathandreandre@gmail.com'
-app.config['MAIL_PASSWORD'] = 'uoowmmbzzvjfrxuo'
+app.config['MAIL_USERNAME'] = '*****'
+app.config['MAIL_PASSWORD'] = '*****'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 
@@ -22,6 +22,6 @@ db = MongoEngine()
 db.init_app(app)
 
 mail = Mail(app)
-
+#uoowmmbzzvjfrxuo
 
 
